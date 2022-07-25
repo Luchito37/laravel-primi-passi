99f70df4a1588cda2cedd8f446136e77f9cfd4c9
Luchito37 <lucamatteotti01@gmail.com>
2022-07-25 16:58:03 +0200

    Creata la scritta HELLO RORLD utillizzando lo style del vecchio web-blade-php solo per un fattore estetico

---
+++ resources/views/home.blade.php
@@ -0,0 +1,67 @@
+@php
+    $title = [
+        'firstword' => 'HELLO',
+        'lastword' => 'WORLD'
+    ]
+@endphp
+
+
+
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>HELLO WORLD</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            
+
+            <div class="content">
+                <div class="title m-b-md">
+                    {{$title['firstword'] . " " . $title['lastword']}}
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
